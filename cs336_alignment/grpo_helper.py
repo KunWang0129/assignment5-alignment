cc52ad8db5b17a447e6270004b709ee71264d6a1
KunWang0129 <103203141+KunWang0129@users.noreply.github.com>
2025-07-25 22:49:32 -0700

    grpo helper

---
+++ cs336_alignment/grpo_helper.py
@@ -0,0 +1,37 @@
+import torch
+from typing import Literal
+
+def compute_group_normalized_rewards(
+    reward_fn,
+    rollout_responses,
+    repeated_ground_truths,
+    group_size,
+    advantage_eps,
+    normalize_by_std,
+    ):
+
+    raw_rewards = []
+
+    # Compute rewards for each response in the group
+    for rollout_response, gt_response in zip(rollout_responses, repeated_ground_truths):
+        curr_reward = reward_fn(rollout_response, gt_response)["reward"]
+        raw_rewards.append(curr_reward)
+    
+    # Compute the mean reward for each group
+    raw_rewards = torch.tensor(raw_rewards)
+    rewards_per_group = raw_rewards.reshape((-1, group_size))
+    mean_reward = torch.mean(rewards_per_group, dim=1, keepdim=True)
+
+    if normalize_by_std:
+        std_reward = torch.std(rewards_per_group, dim=-1, keepdim=True)
+        advandage /= (std_reward + advantage_eps)
+    advantage = advantage.flatten()
+
+    metadata = {
+        'mean': torch.mean(raw_rewards),
+        'std': torch.std(raw_rewards),
+        'max': torch.max(raw_rewards),
+        'min': torch.min(raw_rewards),
+    }
+
+    return advantage, raw_rewards, metadata
--- tests/adapters.py
+++ tests/adapters.py
@@ -9,8 +9,16 @@ from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
 
-from cs336_alignment.sft_helper import tokenize_prompt_and_output, compute_entropy, get_response_log_probs, masked_normalize, sft_microbatch_train_step
-
+from cs336_alignment.sft_helper import (
+    tokenize_prompt_and_output, 
+    compute_entropy, 
+    get_response_log_probs,
+    masked_normalize,
+    sft_microbatch_train_step,
+)
+from cs336_alignment.grpo_helper import (
+    compute_group_normalized_rewards,
+)
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -81,7 +89,14 @@ def run_compute_group_normalized_rewards(
                 You may choose what you wish to log here
                 (some statistics of the rewards, etc.).
     """
-    raise NotImplementedError
+    return compute_group_normalized_rewards(
+        reward_fn=reward_fn,
+        rollout_responses=rollout_responses,
+        repeated_ground_truths=repeated_ground_truths,
+        group_size=group_size,
+        advantage_eps=advantage_eps,
+        normalize_by_std=normalize_by_std,
+    )
 
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
